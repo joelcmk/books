00ead5a91dba3a3025fee2f75cfcb4debd600797
joelcmk <57118300+joelcmk@users.noreply.github.com>
2022-04-05 21:53:43 -0500

    Fetched data form Google books api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,38 @@
+
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+
+  const [data, setData] = useState();
+  const [test, setTest] = useState({});
+
+  useEffect(() => {
+    fetch('https://www.googleapis.com/books/v1/volumes?q=pride+prejudice&download=epub&key')
+      .then(response => response.json())
+      .then(data => setData(data));
+  }, []);
+
+
+
+  if (data) {
+    return (
+      <div className="App">
+        {data.items.map(book => (
+          <div>
+            {book.volumeInfo.title}
+          </div>
+        ))}
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        Loading...
     </div>
-  );
+    )
+  }
+
 }
 
 export default App;
